7c7bcb7b6ea085bcf2776f236592e5f9f4b4c0e4
dynkim <doyeangkim@gmail.com>
2022-06-09 20:29:59 +0900

    테스트입니다

---
+++ Assets/cube_man_CTRL.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class cube_man_CTRL : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+    public float turnSpeed = 80.0f;
+
+    private float h;
+    private float v;
+
+    private void Update()
+    {
+        h = Input.GetAxis("Horizontal");  // -1.0f ~ 1.0f
+        v = Input.GetAxis("Vertical");     // -1.0f ~ 1.0f
+
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime * v);
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * h);
+    }
+}
