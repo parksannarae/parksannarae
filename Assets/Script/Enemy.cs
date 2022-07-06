a752bbe2f04ad8490d65ab50bc68b0ed7941e775
parksannarae <enalchi@naver.com>
2022-07-07 00:48:04 +0900

    음악 작업

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,71 +1,39 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.AI;
-// public class Enemy : MonoBehaviour
-// {
-//     public int maxHealth;
-//     public int curHealth;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 
-//     // public Transform Target;
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth;
+    public int curHealth;
+    public Transform target;
 
-//     Rigidbody rigid;
-//     BoxCollider boxCollider;
-//     Material mat;
-//     NavMeshAgent nav;
+    Rigidbody rigid;
+    BoxCollider boxCollider;
+    Material mat;
+    NavMeshAgent nav;
 
-//     void Awake()
-//     {
-//         rigid = GetComponent<Rigidbody>();
-//         boxCollider = GetComponent<BoxCollider>();
-//         mat = GetComponent<MeshRenderer>().material;
-//         nav = GetComponent<NavMeshAgent>();
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        mat = GetComponentInChildren<MeshRenderer>().material;
+        nav = GetComponent<NavMeshAgent>();
+    }
 
-//     }
+    void Update()
+    {
+        nav.SetDestination(target.position);
+    }
 
-//     void Update()
-//     {
-//        nav.SetDestination(target.position);
-//     }
-
-//     void FreezeVelocity()
-//     {
-//         rigid.velocity = Vector3.zero;
-//         rigid.angularVelocity = Vector3.zero;
-//     }
-
-
-//     void FixedUpdate()
-//     {
-//         FreezeVelocity();
-//     }
-
-//     void OnTriggerEnter(Collider other)
-//     {
-       
-
-//         //원거리 공격(손전등)
-//         if (other.tag == "FlashLight") {
-//             Weapon flashlight = other.GetComponent<FlashLight>();
-//             curHealth -= FlashLight.damage;
-//             StartCoroutine(OnDanmage());
-//            Debug.Log("Range ; " + curHealth);
-//         }
-
-//     }
-            
-//     //피격 로직
-//     IEnumerator OnDanmage()
-//     {
-//         mat.color = Color.red;
-//         yield return new WaitForSeconds(0.1f);
-
-//         if(curHealth > 0) {
-//             mat.color = Color.white;
-//         }
-//         else{
-//             mat.color = Color.gray;
-//             Destroy(gameObject,4);
-//         }
-//     }
-// }
+    void FreezeVelocity()
+    {
+        rigid.velocity = Vector3.zero;
+        rigid.angularVelocity = Vector3.zero;
+    }
+    void FixedUpdate()
+    {
+        FreezeVelocity();
+    }
+}
